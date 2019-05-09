dd63166fdf963365e00aa62f5a867f4198f94c75
Anne Edmondson <edmondson.anne@gmail.com>
2019-05-09 05:24:54 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -36,9 +36,9 @@ def select_youngest_bear_and_returns_name_and_age
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  "SELECT color
-  FROM bears
+  "SELECT color,
   COUNT(color)
+  FROM bears
   GROUP BY color
   ORDER BY count(color)
   DESC
